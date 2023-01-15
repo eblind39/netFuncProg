680c6f84043b81278be94bac08cd25dbfff77bb2
Ernesto Gutierrez <ergutierrez39@gmail.com>
2023-01-14 19:09:43 -0600

    feat: series generation - pythagorean triplets

--- FPSeriesGen/FPSG01_MathStats.cs
+++ FPSeriesGen/FPSG01_MathStats.cs
@@ -4,12 +4,34 @@ public class MathStatistics
 {
     public static void RunSamples()
     {
-        // product of two vectors
+        DotProductVectors();
+        PythagoreanTriplets();
+    }
+
+    static private void DotProductVectors()
+    {
+        Console.WriteLine(">>> dot product of two vectors");
+        // dot product of two vectors
         int[] v1 = new int[]{ 1, 2, 3 }; // First vector
         int[] v2 = new int[]{ 3, 2, 1 }; // Second vector
 
-        // dot product of two vectors
         IEnumerable<int> dotprod = v1.Zip(v2, (a, b) => a * b);
         foreach(int res in dotprod) Console.WriteLine($"{res}");
+    }
+
+    static private void PythagoreanTriplets()
+    {
+        Console.WriteLine(">>> Pythagorean Triplets");
+
+        var pts = Enumerable.Range(2, 4)
+            .Select(c => new {
+                            Length = 2 * c,
+                            Height = c * c - 1,
+                            Hypotenuse = c * c + 1
+                        });
+
+        Console.WriteLine("Length\t\t Height\t\t Hypotenuse:");
+        foreach(var pt in pts) 
+            Console.WriteLine($"{pt.Length}\t\t {pt.Height}\t\t {pt.Hypotenuse}");
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,10 +8,12 @@ public class FProg
 {
     public static void Main(string[] args)
     {
-        ConsoleKeyInfo ki = Console.ReadKey();
-        if (ki.Key == ConsoleKey.D1) Console.WriteLine("Console read was NumPad1");
-        FPInit.RunFPInit();
-        FPSecond.RunFPSecond();
+        // ConsoleKeyInfo ki = Console.ReadKey();
+        // if (ki.Key == ConsoleKey.D1) Console.WriteLine("Console read was NumPad1");
+
+        // FPInit.RunFPInit();
+        // FPSecond.RunFPSecond();
+        
         MathStatistics.RunSamples();
     }
 }
